11c9592e12a9d18dfa1dca4cb8e1f3f50f8a4a83
moody16 <jan.bargiel@gmail.com>
2018-11-23 15:32:14 +0100

    Add update status buttons

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.pajama.trackfoodtruck.Activities;
 
+import com.example.pajama.trackfoodtruck.ListAdapter.ReviewListAdapter;
 import com.example.pajama.trackfoodtruckowner.R;
 
 import android.content.Intent;
@@ -8,11 +9,28 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class WelcomeActivity extends AppCompatActivity
 {
 
 	MenuItem itemToHide;
+	String[] dateArray = {"13 Lipca","23 Lutego","15 Sierpnia","5 Stycznia"};
+
+	String[] authorArray = {
+			"Bradley Henderson",
+			"Efe Peck",
+			"Melanie Nolan",
+			"Jamie-Lee Rennie"
+	};
+
+	String[] reviewArray = {
+			"Wow",
+			"Delicious in rolls",
+			"Great",
+			"Nice"
+	};
+	ListView listView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -20,6 +38,9 @@ public class WelcomeActivity extends AppCompatActivity
 		setContentView(R.layout.activity_welcome);
 		Toolbar myToolbar = (Toolbar) findViewById(R.id.ActivityToolbar);
 		setSupportActionBar(myToolbar);
+		ReviewListAdapter reviewListAdapter = new ReviewListAdapter(this, dateArray, authorArray, reviewArray);
+		listView = (ListView) findViewById(R.id.reviewsList);
+		listView.setAdapter(reviewListAdapter);
 	}
 
 	@Override
