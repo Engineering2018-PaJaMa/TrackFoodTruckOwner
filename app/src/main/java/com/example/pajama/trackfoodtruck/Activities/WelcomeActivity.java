a0498902b85a2611771a909d7b97c4c1c8e5f0ab
moody16 <jan.bargiel@gmail.com>
2018-11-23 14:13:26 +0100

    Add toolbar menu

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/WelcomeActivity.java
@@ -4,6 +4,8 @@ import com.example.pajama.trackfoodtruckowner.R;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class WelcomeActivity extends AppCompatActivity
 {
@@ -13,5 +15,14 @@ public class WelcomeActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		Toolbar myToolbar = (Toolbar) findViewById(R.id.ActivityToolbar);
+		setSupportActionBar(myToolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
 	}
 }
